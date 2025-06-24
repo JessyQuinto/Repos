3cc53f9fa996466bed146edd83d6c48a2935dbe4
JESSY QUINTO TORRES <marianareyesgonzalez4@gmail.com>
2025-06-24 14:16:53 -0500

    update

--- TesorosChoco.Infrastructure/obj/Debug/net9.0/TesorosChoco.Infrastructure.AssemblyInfo.cs
+++ TesorosChoco.Infrastructure/obj/Debug/net9.0/TesorosChoco.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesorosChoco.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b820b192e7b5a6e8a455575c0b1dbcdf77ddbd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e34d24b2f4c6dc827bbacdec3b1bcd875a137685")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesorosChoco.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesorosChoco.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
